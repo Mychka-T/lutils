0462006f70bb7159d05b783239adf064948febb5
Tima <timothymychka@gmail.com>
2020-11-20 16:36:04 +0200

    new repost

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1
+function isOdd(a) {
+  return a % 2 ? "true" : "false"
+}
+console.log(isOdd(7))
+console.log(isOdd(8))
+
+// 2
+function isEven(a) {
+  return a % 2 ? "false" : "true"
+}
+console.log(isEven(5))
+console.log(isEven(4))
+
+// 3
+function sum(a, b) {
+  return 4 + 5
+}
+console.log(sum())
+
+// 4
+function multiple(a, b) {
+  return 4 * 5
+}
+console.log(multiple())
+
+
+
